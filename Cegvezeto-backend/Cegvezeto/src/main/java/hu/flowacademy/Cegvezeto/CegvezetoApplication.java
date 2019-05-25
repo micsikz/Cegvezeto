eb79778faee46b695fb6e714cdafd4b205d36b00
micsikz <micsikzoltan@vipmail.hu>
2019-05-25 10:13:57 +0200

    configure Spring

---
+++ Cegvezeto-backend/Cegvezeto/src/main/java/hu/flowacademy/Cegvezeto/CegvezetoApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.Cegvezeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CegvezetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CegvezetoApplication.class, args);
+	}
+
+}
